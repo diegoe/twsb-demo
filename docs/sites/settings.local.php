36a1ad75ac0bb63fb71644f34f8f4ab0c35df35a
Diego Escalante Urrelo <diego@tag1.com>
2021-11-17 00:28:02 -0500

    composer: Add setup:debug command
    This enables the `*.local.*` files that disable theming caching,
    minification, etc.

---
+++ docs/sites/settings.local.php
@@ -0,0 +1,9 @@
+<?php
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+$settings['cache']['bins']['page'] = 'cache.backend.null';
+
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.local.yml';
