7115307e0ebca8761e757f838e6a012658d11a59
Diego Escalante Urrelo <diego@tag1.com>
2021-11-17 02:43:47 -0500

    WIP: storybook import

---
+++ web/themes/custom/twsb/.storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(json)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
