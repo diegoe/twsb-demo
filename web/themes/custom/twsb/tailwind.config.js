d1000806d6af1e92bc2fadbaa2b121f6a87ba89f
Diego Escalante Urrelo <diego@tag1.com>
2021-11-17 00:28:02 -0500

    theme: Add build/dist commands
    They each consider different development scenarios.

--- web/themes/custom/twsb/tailwind.config.js
+++ web/themes/custom/twsb/tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  purge: [],
+  /* Produce minimal CSS Tailwind files
+   *
+   * When `NODE_ENV` is set to `production`, Tailwind will look in this
+   * array for files that should be used to determine exactly which
+   * classes to include in the final CSS.
+   *
+   * See: https://tailwindcss.com/docs/optimizing-for-production#removing-unused-css
+   */
+  purge: [
+    './templates/**/*.twig',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
