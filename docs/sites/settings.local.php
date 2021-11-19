f8a3e8d51dfd64ffa82e471bff9eb6c286afc95a
Diego Escalante Urrelo <diego@tag1.com>
2021-11-18 22:42:46 -0500

    WIP: Add CORS SB config (unsafe)

--- docs/sites/settings.local.php
+++ docs/sites/settings.local.php
@@ -7,3 +7,8 @@ $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 $settings['cache']['bins']['page'] = 'cache.backend.null';
 
 $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.local.yml';
+
+/* Use Storybook settings, if available */
+if (file_exists($app_root . '/' . $site_path . '/services.storybook.yml')) {
+  $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.storybook.yml';
+}
